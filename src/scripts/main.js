8ebc317a68096d6d8e42f141f05122dcbb4d5f45
Aliaksei Zhukouski <a.zhukouski@softarex.com>
2022-02-17 20:33:30 +0300

    coding desktop + mob

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+  // Add smooth scrolling to all links
   $(".reviews__slider").slick({
     slidesToShow: 5,
     slidesToScroll: 1,
@@ -12,48 +14,80 @@ $(document).ready(function () {
     focusOnSelect: true,
     responsive: [
       {
-        breakpoint: 769,
+        breakpoint: 1200,
         settings: {
-          fade: true,
-          arrows: false,
-          slidesToShow: 1,
+          slidesToShow: 3,
         },
       },
     ],
   });
-});
 
-$(".reviews__phone-slider").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  draggable: false,
-  speed: 400,
-  dots: false,
-  arrows: false,
-  asNavFor: '.reviews__slider',
-  responsive: [
-    {
-      breakpoint: 769,
-      settings: {
-        fade: true,
-        arrows: false,
-        slidesToShow: 1,
-      },
-    },
-  ],
-});
 
-$('.reviews__phone-slider-btn--left').click(function () {
-  $('.reviews__phone-slider').slick('slickPrev');
-});
-$('.reviews__phone-slider-btn--right').click(function () {
-  $('.reviews__phone-slider').slick('slickNext');
-});
+  $(".reviews__phone-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: false,
+    speed: 400,
+    dots: false,
+    arrows: false,
+    asNavFor: '.reviews__slider',
+  });
 
-new ScrollBooster({
-  viewport: document.querySelector('.roadmap__wrap'),
-  scrollMode: 'transform',
-  direction: "horizontal",
-  pointerMode: "all"
-});
+  $('.reviews__phone-slider-btn--left').click(function () {
+    $('.reviews__phone-slider').slick('slickPrev');
+  });
+  $('.reviews__phone-slider-btn--right').click(function () {
+    $('.reviews__phone-slider').slick('slickNext');
+  });
 
+  const sb = new ScrollBooster({
+    viewport: document.querySelector('.roadmap__wrap'),
+    scrollMode: 'transform',
+    direction: "horizontal",
+    pointerMode: "all",
+    bounce: 'false',
+  });
+
+  let roadmapWidth = $('.roadmap__flex').width(),
+    currentWidth = 0;
+
+  setTimeout(function () {
+    roadmapWidth = $('.roadmap__flex').width();
+  }, 5000)
+
+  let timerId = setInterval( function () {
+    if(currentWidth < roadmapWidth) {
+      currentWidth+= 0.05;
+      sb.scrollTo({ x: currentWidth, y: 0 });
+    } else {
+      sb.scrollTo({ x: 0, y: 0 });
+      currentWidth = 0;
+    }
+  }, 1);
+
+  $('.roadmap__flex').click(function () {
+    clearTimeout(timerId);
+    sb.updateOptions({ bounce: true });
+  })
+  $('.roadmap__flex').bind('touchstart', function () {
+    clearTimeout(timerId);
+    sb.updateOptions({ bounce: true });
+  })
+
+
+  $('.header__hamburger-wrap').click(function (){
+    $(this).toggleClass('active');
+    $('.mob-menu').toggleClass('active');
+  })
+
+  $('.nav__item').click(function (){
+    $('.header__hamburger-wrap').removeClass('active');
+    $('.mob-menu').removeClass('active');
+  })
+
+  $('.mob-menu__close-btn-wrap').click(function (){
+    $('.header__hamburger-wrap').removeClass('active');
+    $('.mob-menu').removeClass('active');
+  })
+
+});
